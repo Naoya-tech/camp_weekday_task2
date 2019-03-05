2bb7b10d254db15541ecbf2405340c2c3baeabb9
Naoya-tech <naoya.techwork@gmail.com>
2019-03-05 12:06:08 +0900

    exercise02-2

--- car.rb
+++ car.rb
@@ -44,3 +44,13 @@ class Car
     @@count  # return @@countの略
   end
 end
+
+class TrackCar < Car
+
+attr_reader :load_weight
+
+def initialize(number,color,load_weight="500")
+  @load_weight = load_weight
+end
+
+end
